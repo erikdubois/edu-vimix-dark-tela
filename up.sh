e7c4ebcd5b2ab187481e52fc3c6ab615826ca5fc
Erik Dubois <erik.dubois@gmail.com>
2025-04-14 20:44:06 +0200

    udpate

--- up.sh
+++ up.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 #set -e
 ##################################################################################################################
-# Author 	: Erik Dubois
+# Author    : Erik Dubois
 # Website   : https://www.erikdubois.be
-# Website	: https://www.arcolinux.info
-# Website	: https://www.arcolinux.com
-# Website	: https://www.arcolinuxd.com
-# Website	: https://www.arcolinuxb.com
-# Website	: https://www.arcolinuxiso.com
-# Website	: https://www.arcolinuxforum.com
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################
+
+# reset - commit your changes or stash them before you merge
+# git reset --hard - personal alias - grh
 
 # checking if I have the latest files from github
 echo "Checking for newer files online first"
@@ -21,8 +28,6 @@ git pull
 
 # Below command will backup everything inside the project folder
 git add --all .
-
-#rm /home/erik/DATA/edu-papirus-dark-tela/usr/share/icons/Papirus-Dark-Tela/icon-theme.cache
 
 # Give a comment to the commit if you want
 echo "####################################"
